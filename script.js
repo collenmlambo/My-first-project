4525acd8f5d5fd0a38d67a72ed0746f05582d2bf
Collen <128287445+collenmlambo@users.noreply.github.com>
2023-05-20 12:42:34 +0200

    Add files via upload
    quiz i created using html css and java script

---
+++ script.js
@@ -0,0 +1,394 @@
+const questions = [
+    {
+
+        question: "What is the largest animal in the world?",
+        answers: [
+            {text: "shark",correct:false},
+            {text: "Blue whale",correct:true},
+            {text: "Elephant",correct:false},
+            {text: "Giraffe",correct:false},
+        ]
+    },
+    {
+        question: "Which one is the best college in south africa?",
+        answers: [
+            {text: "Ewc",correct:false},
+            {text: "Rock of springs technical college",correct:false},
+            {text: "Ekhuruleni city Collge",correct:true},
+            {text: "boston",correct:false},
+        ]
+
+    },
+    {
+        question: "What is the smallest country in the world?",
+        answers: [
+            {text: "Vatican city",correct:true},
+            {text: "Zimmbabwe",correct:false},
+            {text: "Nepal",correct:false},
+            {text: "Shri lanka",correct:false},
+        ]
+
+
+
+    },
+    {
+        question: "What is the smallest continent in the world?",
+        answers: [
+            {text: "Asia",correct:false},
+            {text: "Artic",correct:false},
+            {text: "Australia",correct:true},
+            {text: "Africa",correct:false},
+        ]
+    },
+    {
+
+        question: "Who is the Principal of Ekhuruleni city college?",
+        answers: [
+            {text: "Mr mofokeng",correct:false},
+            {text: "MR Malauda",correct:true},
+            {text: "MRS Jacobs",correct:false},
+            {text: "MR Patros",correct:false},
+        ]
+    },
+    {
+
+        question: "What is the Name of the Tallest mountain in the world?",
+        answers: [
+            {text: "K2",correct:false},
+            {text: "kilaminjaro",correct:false},
+            {text: "Mount Everest ",correct:true},
+            {text: "Mount Katherine",correct:false},
+        ]
+
+    },
+    {
+
+        question: "What is the name of the second tallest mountain in the world?",
+        answers: [
+            {text: "Mount everest",correct:false},
+            {text: "K2",correct:true},
+            {text: "kilaminjaro",correct:false},
+            {text: "mount katherine",correct:false},
+        ]
+
+    },
+    {
+
+        question: "Who is the minister of Eskom?",
+        answers: [
+            {text: "Bheki cele",correct:false},
+            {text: "Kgosientsho Sputla",correct:true},
+            {text: "Enoch Godongwana",correct:false},
+            {text: "Dr Phaala",correct:false},
+        ]
+    },
+    {
+        question: "Who won the world cup in 2022?",
+        answers: [
+            {text: "France",correct:false},
+            {text: "Morocco",correct:false},
+            {text: "South africa",correct:false},
+            {text: "Argentina",correct:true},
+        ]
+
+    },
+    {
+
+        question: "Who won the player of the tournament in the 2022 world cup?",
+        answers: [
+            {text: "Lionel Messi",correct:true},
+            {text: "Kylian Mbampe",correct:false},
+            {text: "Enzo Fernandez",correct:false},
+            {text: "Romeo Lukaku",correct:false},
+        ]
+    },
+    {
+
+        question: "What is the name of the Person who create this quiz?",
+        answers: [
+            {text: "Calvin Spence",correct:false},
+            {text: "Carlos Junior",correct:false},
+            {text: "Collen Mlambo",correct:true},
+            {text: "Thando Mabuza",correct:false},
+        ]
+    },
+    {
+        question: "What is the biggest country in the world?",
+        answers: [
+            {text: "Japan",correct:false},
+            {text: "NEW Zealand",correct:false},
+            {text: "USA",correct:false},
+            {text: "China",correct:true},
+        ]
+    },
+    {
+        question: "What is the capital city of south africa?",
+        answers: [
+            {text: "Pretoria",correct:true},
+            {text: "Sandton",correct:false},
+            {text: "Johanesburg",correct:false},
+            {text: "Cape town",correct:false},
+        ]
+    },
+    {
+        question: "What is the closest planet to the sun?",
+        answers: [
+            {text: "Mars ",correct:false},
+            {text: "Earth ",correct:false},
+            {text: "Jupitor",correct:false},
+            {text: "Mecury",correct:true},
+        ]
+    },
+    {
+        question: "Who is the best soccer player in the world?",
+        answers: [
+            {text: " Lionel Messi",correct:true},
+            {text: "Phele",correct:false},
+            {text: "Diego Maradona",correct:false},
+            {text: "Cristiano Ronaldo",correct:false},
+        ]
+    },
+    {
+        question: "Who is the president of zimbabwe?",
+        answers: [
+            {text: "Robert mugabe ",correct:false},
+            {text: "Jacob Zuma",correct:false},
+            {text: " Ermeson Munangagwa",correct:true},
+            {text: "Julius Malema",correct:false},
+        ]
+    },
+    {
+        question: "What province is kimberly located in?",
+        answers: [
+            {text: "Gauteng",correct:false},
+            {text: "Free State",correct:false},
+            {text: "North west",correct:false},
+            {text: "Northen cape",correct:true},
+        ]
+    },
+    {
+        
+        question: "How many seconds are in a day?",
+        answers: [
+            {text: "86350",correct:false},
+            {text: "86400",correct:true},
+            {text: "86700",correct:false},
+            {text: "86416",correct:false},
+        ]
+
+
+    },
+    {
+
+        question: "What is the capital city of England?",
+        answers: [
+            {text: "Manchester united",correct:false},
+            {text: "Manchester city",correct:false},
+            {text: "London",correct:true},
+            {text: "Bermingham",correct:false},
+        ]
+
+    },
+    {
+
+        question: "What is the most viewed african song on YOUTUBE?",
+        answers: [
+            {text: "JERUSALEMA",correct:true},
+            {text: "DECSPACITO ",correct:false},
+            {text: "KILAMINJARO",correct:false},
+            {text: "LAST LAST",correct:false},
+        ]
+
+
+    },
+    {
+
+
+
+        question: " Who is the Undefeated boxing legend?",
+        answers: [
+            {text: "Mohamed Alli",correct:false},
+            {text: "Errol spence jr",correct:false},
+            {text: "Mike tyson",correct:false},
+            {text: "Floyd Mayweather",correct:true},
+        ]
+    },
+    {
+
+        question: "How many continets in the world?",
+        answers: [
+            {text: "7",correct:true},
+            {text: "8",correct:false},
+            {text: "6",correct:false},
+            {text: "7.5",correct:false},
+        ]
+        
+    },
+    {
+
+        question: "What is the colour of the sun?",
+        answers: [
+            {text: "yellow",correct:false},
+            {text: "red",correct:false},
+            {text: "orange",correct:true},
+            {text: "black",correct:false},
+        ]
+    },
+    {
+
+        question: "When was the covid 19 outbreake declared a national disaster?",
+        answers: [
+            {text: "2019",correct:false},
+            {text: "2021",correct:false},
+            {text: "2022",correct:false},
+            {text: "2020",correct:true},
+        ]
+    },
+    {
+        question: "Who has more individual awards?",
+        answers: [
+            {text: "cristiano ronaldo",correct:false},
+            {text: "Ronaldo",correct:false},
+            {text: " Erling Halaand",correct:false},
+            {text: "Leonel Messi",correct:true},
+        ]
+    },
+    {
+
+        question: "Who is the president of  USA?",
+        answers: [
+            {text: "Joe biden",correct:true},
+            {text: "Donald trump",correct:false},
+            {text: "Barack obama",correct:false},
+            {text: "Cyrial Ramaphosa",correct:false},
+        ]
+    },
+    {
+
+        question: "Who is currently the richest man in the world?",
+        answers: [
+            {text: "Bill gates ",correct:false},
+            {text: "Elon Musk",correct:true},
+            {text: "Jeff Bezzos",correct:false},
+            {text: "Fabrizio Romano",correct:false},
+        ]
+    },
+    {
+
+        question: "Who started microsoft?",
+        answers: [
+            {text: "Bill Gates",correct:true},
+            {text: "Elon musk",correct:false},
+            {text: "Mark zuckerberg",correct:false},
+            {text: "Jhon spence",correct:false},
+        ]
+    },
+    {
+
+        question: "What year was the first iphone relesed?",
+        answers: [
+            {text: "1999",correct:false},
+            {text: "2005",correct:false},
+            {text: "2004",correct:false},
+            {text: "2007",correct:true},
+        ]
+    },
+    {
+        question: "What year was the first Android released?",
+        answers: [
+            {text: "1992",correct:false},
+            {text: "2006",correct:false},
+            {text: "2008",correct:true},
+            {text: "2007",correct:false},
+        ]
+    }
+    
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". "+ currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer=>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+function selectAnswer (e){
+    const selectedBtn = e.target;
+    const iscorrect = selectedBtn.dataset.correct === "true";
+    if(iscorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    nextButton.style.display = "block";
+
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display ="block";
+}
+
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if (currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+startQuiz();
